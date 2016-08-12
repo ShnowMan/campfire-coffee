adc67312093a43e9e27e34c2e4cdf8022cc49ece
Jeff King <king.jeffey@gmail.com>
2016-08-11 17:59:57 -0700

    got majority of object functioning. still need employees

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var pikePlaceMarket = {
   customerPerHour : [],
   cupPerHour : [],
   poundPerHour : [],
+  customerTotal: 0,
   cupTotal : 0,
   poundTotal : 0,
   randomNum : function(min, max) {
@@ -15,25 +16,25 @@ var pikePlaceMarket = {
     for (var i = 0; i < 15; i++) {
       var oneHourOfCustomers = this.randomNum(this.maxCustomer, this.minCustomer);
       this.customerPerHour.push(oneHourOfCustomers);
+      this.customerTotal += oneHourOfCustomers;
     }
   },
   generateCupPerHour : function() {
     for (var i = 0; i < this.customerPerHour.length; i++) {
+      var oneHourOfCups = (this.customerPerHour[i] * this.averageCup);
       this.cupPerHour.push(this.customerPerHour[i] * this.averageCup);
+      this.cupTotal += oneHourOfCups;
     };
   },
-  // generatePoundPerHour : function() {
-  //   var poundPerCus = (oneHourOfCustomers * this.averagePound) + (cupPerCus / 16);
-  //   for (var i = 0; i < 15; i ++){
-  //     this.poundPerHour[i] += poundPerCus;
-  //   };
-  // },
   generatePoundPerHour : function() {
     for (var i = 0; i < this.customerPerHour.length; i++){
-      this.poundPerHour.push((this.customerPerHour[i] * this.averagePound) + (cupPerCus / 16));
+      var oneHourOfPounds = (this.customerPerHour[i] * this.averagePound) + (this.cupPerHour[i] / 16);
+      this.poundPerHour.push((this.customerPerHour[i] * this.averagePound) + (this.cupPerHour[i] / 16));
+      this.poundTotal += oneHourOfPounds;
     };
   },
 };
 pikePlaceMarket.generateCustomerPerHour();
 pikePlaceMarket.generateCupPerHour();
 pikePlaceMarket.generatePoundPerHour();
+console.log(pikePlaceMarket);
