3ba73aa241ad136b502593a18d57190c04fe8ea5
Jeff King <king.jeffey@gmail.com>
2016-08-14 16:05:11 -0700

    First table created. Need blank space on table header. Need total row

--- app.js
+++ app.js
@@ -1,76 +1,9 @@
-/*
-var pikePlaceMarket = {
-  name: 'Pike Place Market',
-  minCustomer: 14,
-  maxCustomer: 35,
-  averageCup: 1.2,
-  averagePound: 0.34,
-  customerPerHour : [],
-  cupPerHour : [],
-  poundsForCupsHourly: [],
-  poundToGoHour : [],
-  poundOverAllHourly : [],
-  customerTotal: 0,
-  cupTotal : 0,
-  poundsForCupsTotal : 0,
-  poundToGoTotal : 0,
-  poundOverAllTotal : 0,
-  empNeeded: [],
-  randomNum : function(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  },
-  generateCustomerPerHour : function() {
-    for (var i = 0; i < timeOpen.length; i++) {
-      var oneHourOfCustomers = this.randomNum(this.maxCustomer, this.minCustomer);
-      this.customerPerHour.push(oneHourOfCustomers);
-      this.customerTotal += oneHourOfCustomers;
-    }
-  },
-  generateCupPerHour : function() {
-    for (var i = 0; i < timeOpen.length; i++) {
-      var oneHourOfCups = parseFloat((this.customerPerHour[i] * this.averageCup).toFixed(2));
-      this.cupPerHour.push(oneHourOfCups);
-      this.cupTotal += oneHourOfCups;
-    };
-  },
-  generatePoundPerHour : function() {
-    for (var i = 0; i < timeOpen.length; i++){
-      var oneHourOfPounds = parseFloat((this.customerPerHour[i] * this.averagePound).toFixed(2));
-      this.poundToGoHour.push(oneHourOfPounds);
-      this.poundToGoTotal += oneHourOfPounds;
-      var oneHourOfCupPounds = parseFloat((this.cupPerHour[i] / 16).toFixed(2));
-      this.poundsForCupsHourly.push(oneHourOfCupPounds);
-      this.poundsForCupsTotal += oneHourOfCupPounds;
-      var totalPoundsCombined = parseFloat((this.customerPerHour[i] * this.averagePound).toFixed(2)) + parseFloat((this.cupPerHour[i] / 16).toFixed(2));
-      this.poundOverAllHourly.push(totalPoundsCombined);
-      this.poundOverAllTotal += totalPoundsCombined;
-    };
-  },
-  generateEmployeeNeeded : function () {
-    for (var i = 0; i < timeOpen.length; i++)
-      if ((this.customerPerHour[i] / 30) >= 1) {
-        this.empNeeded[i] = 2;
-      } else {
-        this.empNeeded[i] = 1;
-      }
-  },
-  render: function() {
-    this.generateCustomerPerHour();
-    this.generateCupPerHour();
-    this.generatePoundPerHour();
-    this.generateEmployeeNeeded();
-  },
-};
-console.log(pikePlaceMarket);
-pikePlaceMarket.render();
-*/
-
 var timeOpen =
-['6am','7am','8am',
-'9am','10am','11am',
-'12pm','1pm','2pm',
-'3pm','4pm','5pm',
-'6pm','7pm','8pm',];
+['6:00am','7:00am','8:00am',
+'9:00am','10:00am',':0011am',
+'12:00pm','1:00pm','2:00pm',
+'3:00pm','4:00pm','5:00pm',
+'6:00pm','7:00pm','8:00pm',];
 
 var coffeeShops = [];
 console.log(coffeeShops);
@@ -138,7 +71,7 @@ CoffeeShop.prototype.render = function() {
   this.generatePoundPerHour();
   this.generateEmployeeNeeded();
 };
-console.log(CoffeeShop);
+
 
 new CoffeeShop('Pike Place Market', 14, 35, 1.2, 0.34);
 new CoffeeShop('Capitol Hill', 12, 28, 3.2, 0.03);
@@ -152,3 +85,29 @@ var renderCoffeeShops = function () {
   }
 };
 renderCoffeeShops();
+
+var tableEl = document.getElementById('cfc-data-table');
+function buildTableHeader() {
+  var trEl = document.createElement('tr');
+  for (var i = 0; i < timeOpen.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = timeOpen[i];
+    trEl.appendChild(thEl);
+  }
+  tableEl.appendChild(trEl);
+};
+buildTableHeader();
+
+function buildTableBody() {
+  for (var i = 0; i < coffeeShops.length; i++){
+    var trEl = document.createElement('tr');
+    trEl.textContent = coffeeShops[i].name;
+    for (var a = 0; a < timeOpen.length; a++) {
+      var thEl = document.createElement('th');
+      thEl.textContent = coffeeShops[i].poundOverAllHourly[a].toFixed(2);
+      trEl.appendChild(thEl);
+    }
+    tableEl.appendChild(trEl);
+  }
+};
+buildTableBody();
